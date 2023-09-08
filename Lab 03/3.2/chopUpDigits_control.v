09dfb8301416a36d971a660c7b0430917eca4e98
Mr-Sheep <24599209+Mr-Sheep@users.noreply.github.com>
2023-09-07 19:25:40 -0500

    correctly handling `select_base`

--- Lab 03/3.2/chopUpDigits_control.v
+++ Lab 03/3.2/chopUpDigits_control.v
@@ -4,18 +4,15 @@ module chopUpDigits_control (done, get_new, load_index, load_number, select_base
 	input base, number_is_0, go;
 	input clk, reset;
 
-	assign temp_base = base;
-	assign temp_go = go;
-
 	wire sGarbage, sStart, sWhile8, sWhile16, sWrite8, sWrite16, sDone;
 
-	wire sGarbage_next = (sGarbage & ~temp_go) | reset;
-	wire sStart_next = ((sGarbage & temp_go) | (sStart & temp_go) | (sDone & temp_go)) & ~reset;
-	wire sWhile8_next = ((sStart & (~temp_go & ~temp_base)) | sWrite8) & ~reset;
-	wire sWhile16_next = ((sStart & (~temp_go & temp_base)) | sWrite16) & ~reset;
+	wire sGarbage_next = (sGarbage & ~go) | reset;
+	wire sStart_next = ((sGarbage & go) | (sStart & go) | (sDone & go)) & ~reset;
+	wire sWhile8_next = ((sStart & (~go & ~base)) | sWrite8) & ~reset;
+	wire sWhile16_next = ((sStart & (~go & base)) | sWrite16) & ~reset;
 	wire sWrite8_next = (sWhile8 & ~number_is_0) & ~reset;
 	wire sWrite16_next = (sWhile16 & ~number_is_0) & ~reset;
-	wire sDone_next = ((sWhile8 & number_is_0) | (sWhile16 & number_is_0) | (sDone & ~temp_go)) & ~reset;
+	wire sDone_next = ((sWhile8 & number_is_0) | (sWhile16 & number_is_0) | (sDone & ~go)) & ~reset;
 
 	dffe fsGarbage(sGarbage, sGarbage_next, clk, 1'b1, 1'b0);
 	dffe fsStart(sStart, sStart_next, clk, 1'b1, 1'b0);
@@ -28,9 +25,8 @@ module chopUpDigits_control (done, get_new, load_index, load_number, select_base
 	assign load_number = (sStart | sWrite8 | sWrite16);
 	assign load_index = (sStart | sWrite8 | sWrite16);
 	assign get_new = sStart;
-    assign select_base = temp_base;
-    assign write = (sWrite8 | sWrite16);
+  assign select_base = sWrite16;
+  assign write = (sWrite8 | sWrite16);
 
-	// chopUpDigits_circuit c1(number_is_0, get_new, load_index, load_number, select_temp_base, write, number, digits_ptr, clk, reset);
 	assign done = sDone;
 endmodule
